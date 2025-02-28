be7c3563d828089524206073fae5278fdcf39643
21-NOK <nolan.kohli@ceff.ch>
2025-02-28 10:27:57 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,7 @@ enum NeoPixelMode {
  * Functions to operate NeoPixel strips.
  */
 //% weight=5 color=#0000FF icon="\uf110"
-namespace fonction {
+namespace LED {
     /**
      * A NeoPixel strip
      */
@@ -165,7 +165,7 @@ namespace fonction {
                 for (let i = 0; i < n; ++i) {
                     if (i <= v) {
                         const b = Math.idiv(i * 255, n1);
-                        this.setPixelColor(i, fonction.rgb(b, 0, 255 - b));
+                        this.setPixelColor(i, LED.rgb(b, 0, 255 - b));
                     }
                     else this.setPixelColor(i, 0);
                 }
@@ -591,5 +591,164 @@ namespace fonction {
         Clockwise,
         CounterClockwise,
         Shortest
+    }
+}
+
+
+//% color="#FFAB00" weight=100
+namespace distance {
+    let i2cAddr = 0x29;
+    let setting: number[] = []
+
+    function writeRegister16(regAddr: number, value: number) {
+        let buffer = pins.createBuffer(3);
+        buffer[0] = (regAddr >> 8) & 0xFF; // MSB
+        buffer[1] = regAddr & 0xFF;       // LSB
+        buffer[2] = value & 0xFF;         // Valeur
+        pins.i2cWriteBuffer(i2cAddr, buffer);
+    }
+
+    function readRegister(regAddr: number, octet: number): Buffer {
+        let buffer = pins.createBuffer(2);
+        buffer[0] = (regAddr >> 8) & 0xFF; // MSB
+        buffer[1] = regAddr & 0xFF;       // LSB
+        pins.i2cWriteBuffer(i2cAddr, buffer);
+        return pins.i2cReadBuffer(i2cAddr, octet);
+    }
+
+    function getID(): number {
+        let result = readRegister(0x010F, 1); //retoune 0xEB = 235
+        return (result[0]);
+    }
+
+
+    //% block
+    export function init() {
+        while (getID() != 235) {
+            writeRegister16(0x0000, 0x01); // Soft reset
+            basic.pause(100); // Attendre que le capteur redémarre
+
+        }
+        // Exemple d'initialisation de base
+        writeRegister16(0x2E, 0x01);        // "0x2e : bit 0 if I2C pulled up at 1.8V, else set bit 0 to 1 (pull up at AVDD)"
+        writeRegister16(0x2F, 0x01);        // "0x2f : bit 0 if GPIO pulled up at 1.8V, else set bit 0 to 1 (pull up at AVDD)"
+
+        setting =
+            [
+                0x11, /* 0x30 : set bit 4 to 0 for active high interrupt and 1 for active low
+    (bits 3:0 must be 0x1), use SetInterruptPolarity() */
+                0x02, /* 0x31 : bit 1 = interrupt depending on the polarity,
+    use CheckForDataReady() */
+                0x00, /* 0x32 : not user-modifiable */
+                0x02, /* 0x33 : not user-modifiable */
+                0x08, /* 0x34 : not user-modifiable */
+                0x00, /* 0x35 : not user-modifiable */
+                0x08, /* 0x36 : not user-modifiable */
+                0x10, /* 0x37 : not user-modifiable */
+                0x01, /* 0x38 : not user-modifiable */
+                0x01, /* 0x39 : not user-modifiable */
+                0x00, /* 0x3a : not user-modifiable */
+                0x00, /* 0x3b : not user-modifiable */
+                0x00, /* 0x3c : not user-modifiable */
+                0x00, /* 0x3d : not user-modifiable */
+                0xff, /* 0x3e : not user-modifiable */
+                0x00, /* 0x3f : not user-modifiable */
+                0x0F, /* 0x40 : not user-modifiable */
+                0x00, /* 0x41 : not user-modifiable */
+                0x00, /* 0x42 : not user-modifiable */
+                0x00, /* 0x43 : not user-modifiable */
+                0x00, /* 0x44 : not user-modifiable */
+                0x00, /* 0x45 : not user-modifiable */
+                0x20, /* 0x46 : interrupt configuration 0->level low detection, 1-> level high,
+    2-> Out of window, 3->In window, 0x20-> New sample ready , TBC */
+                0x0b, /* 0x47 : not user-modifiable */
+                0x00, /* 0x48 : not user-modifiable */
+                0x00, /* 0x49 : not user-modifiable */
+                0x02, /* 0x4a : not user-modifiable */
+                0x14, /* 0x4b : not user-modifiable */
+                0x21, /* 0x4c : not user-modifiable */
+                0x00, /* 0x4d : not user-modifiable */
+                0x00, /* 0x4e : not user-modifiable */
+                0x05, /* 0x4f : not user-modifiable */
+                0x00, /* 0x50 : not user-modifiable */
+                0x00, /* 0x51 : not user-modifiable */
+                0x00, /* 0x52 : not user-modifiable */
+                0x00, /* 0x53 : not user-modifiable */
+                0xc8, /* 0x54 : not user-modifiable */
+                0x00, /* 0x55 : not user-modifiable */
+                0x00, /* 0x56 : not user-modifiable */
+                0x38, /* 0x57 : not user-modifiable */
+                0xff, /* 0x58 : not user-modifiable */
+                0x01, /* 0x59 : not user-modifiable */
+                0x00, /* 0x5a : not user-modifiable */
+                0x08, /* 0x5b : not user-modifiable */
+                0x00, /* 0x5c : not user-modifiable */
+                0x00, /* 0x5d : not user-modifiable */
+                0x01, /* 0x5e : not user-modifiable */
+                0xcc, /* 0x5f : not user-modifiable */
+                0x07, /* 0x60 : not user-modifiable */
+                0x01, /* 0x61 : not user-modifiable */
+                0xf1, /* 0x62 : not user-modifiable */
+                0x05, /* 0x63 : not user-modifiable */
+                0x00, /* 0x64 : Sigma threshold MSB (mm in 14.2 format for MSB+LSB),
+    use SetSigmaThreshold(), default value 90 mm  */
+                0xa0, /* 0x65 : Sigma threshold LSB */
+                0x00, /* 0x66 : Min count Rate MSB (MCPS in 9.7 format for MSB+LSB),
+    use SetSignalThreshold() */
+                0x80, /* 0x67 : Min count Rate LSB */
+                0x08, /* 0x68 : not user-modifiable */
+                0x38, /* 0x69 : not user-modifiable */
+                0x00, /* 0x6a : not user-modifiable */
+                0x00, /* 0x6b : not user-modifiable */
+                0x00, /* 0x6c : Intermeasurement period MSB, 32 bits register,
+    use SetIntermeasurementInMs() */
+                0x00, /* 0x6d : Intermeasurement period */
+                0x0f, /* 0x6e : Intermeasurement period */
+                0x89, /* 0x6f : Intermeasurement period LSB */
+                0x00, /* 0x70 : not user-modifiable */
+                0x00, /* 0x71 : not user-modifiable */
+                0x00, /* 0x72 : distance threshold high MSB (in mm, MSB+LSB),
+    use SetD:tanceThreshold() */
+                0x00, /* 0x73 : distance threshold high LSB */
+                0x00, /* 0x74 : distance threshold low MSB ( in mm, MSB+LSB),
+    use SetD:tanceThreshold() */
+                0x00, /* 0x75 : distance threshold low LSB */
+                0x00, /* 0x76 : not user-modifiable */
+                0x01, /* 0x77 : not user-modifiable */
+                0x07, /* 0x78 : not user-modifiable */
+                0x05, /* 0x79 : not user-modifiable */
+                0x06, /* 0x7a : not user-modifiable */
+                0x06, /* 0x7b : not user-modifiable */
+                0x00, /* 0x7c : not user-modifiable */
+                0x00, /* 0x7d : not user-modifiable */
+                0x02, /* 0x7e : not user-modifiable */
+                0xc7, /* 0x7f : not user-modifiable */
+                0xff, /* 0x80 : not user-modifiable */
+                0x9B, /* 0x81 : not user-modifiable */
+                0x00, /* 0x82 : not user-modifiable */
+                0x00, /* 0x83 : not user-modifiable */
+                0x00, /* 0x84 : not user-modifiable */
+                0x01, /* 0x85 : not user-modifiable */
+                0x00, /* 0x86 : clear interrupt, use ClearInterrupt() */
+                0x40  /* 0x87 : start ranging, If you want an automatic start after init() call,
+      put 0x40 in location 0x87 */
+            ]
+
+        for (let i = 0x30; i <= 0x87; i++) {
+            writeRegister16(i, setting[i - 0x30]);
+            basic.pause(10);
+        }
+
+
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function getDistance(): number {
+        let result = readRegister(0x96, 2);
+        return (result[0] << 8) | result[1];
     }
 }
